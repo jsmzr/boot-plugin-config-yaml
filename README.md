be0b8fce3665e3dc64a78e82b6dea1bdb44d9f76
jiurongzhao <1053668864@qq.com>
2022-04-09 16:06:07 +0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# bootstrap-plugin-yaml
+
---
+++ yaml/plugin.go
@@ -0,0 +1,18 @@
+package yaml
+
+import (
+	_ "github.com/jsmzr/bootstrap-config-yaml/yaml"
+	"github.com/jsmzr/bootstrap-config/config"
+	"github.com/jsmzr/bootstrap-plugin/plugin"
+)
+
+type YamlConfigPlugin struct {
+}
+
+func (p *YamlConfigPlugin) Load() error {
+	return config.InitInstance("yaml", "application.yaml")
+}
+
+func init() {
+	plugin.Register("config", &YamlConfigPlugin{})
+}
